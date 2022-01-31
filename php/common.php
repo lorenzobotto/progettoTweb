7bc357a02f55ac2c2dc24f6094c3534d5542ce31
Lorenzo Botto <lorenzo.botto163@edu.unito.it>
2022-01-31 14:26:45 +0100

    Connection database

--- php/common.php
+++ php/common.php
@@ -1,6 +1,6 @@
 <?php
     function connect_database(){
-        $connectstr = "mysql:dbname=dailyticket;host=eu-cdbr-west-02.cleardb.net";
+        $connectstr = "mysql:dbname=heroku_ee44f4558f1cfea;host=eu-cdbr-west-02.cleardb.net";
         try {
             $db = new PDO($connectstr, "b0523d51feff63", "3e6a2459");
         } catch(PDOException $ex){
