1332fabb1d1b30ad7f6bc73fba3ca68dbd58d072
Lorenzo Botto <lorenzo.botto163@edu.unito.it>
2022-01-31 13:58:42 +0100

    Modifiche connessione database

--- php/common.php
+++ php/common.php
@@ -1,8 +1,8 @@
 <?php
     function connect_database(){
-        $connectstr = "mysql:dbname=dailyticket;host=localhost:3306";
+        $connectstr = "mysql:dbname=dailyticket;host=eu-cdbr-west-02.cleardb.net";
         try {
-            $db = new PDO($connectstr, "root", "");
+            $db = new PDO($connectstr, "b0523d51feff63", "3e6a2459");
         } catch(PDOException $ex){
             die('Could not connect: ' . $ex->getMessage());
         }
