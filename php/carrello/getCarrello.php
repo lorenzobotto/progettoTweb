7b9d223c229d3cd43e4b46b9970cc5f0187a40c9
Lorenzo Botto <lorenzo.botto163@edu.unito.it>
2022-01-31 20:47:42 +0100

    Changes

--- php/carrello/getCarrello.php
+++ php/carrello/getCarrello.php
@@ -40,7 +40,11 @@
             while($row = $idsDb->fetch()){
                 $ids = $ids . $db->quote($row['idEvento']) . ",";
             }
-            $cart = $db->prepare("SELECT * FROM eventi WHERE id IN (" . substr($ids, 0, -1) . ") ORDER BY FIELD(id, " . substr($ids, 0, -1) . ")");
+            if ($ids == "") {
+                $cart = $db->prepare("SELECT * FROM eventi WHERE id IN (" . substr($ids, 0, -1) . ")");
+            } else {
+                $cart = $db->prepare("SELECT * FROM eventi WHERE id IN (" . substr($ids, 0, -1) . ") ORDER BY FIELD(id, " . substr($ids, 0, -1) . ")");
+            }
             $cart->execute();
         } catch(PDOException $ex){
             die('Error in making query: ' . $ex->getMessage());
